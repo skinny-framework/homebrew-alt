b392b5fae052975a41905cb72d2ba03c3ac7a953
Kazuhiro Sera <seratch@gmail.com>
2014-08-24 14:37:08 +0900

    Fix install error

--- skinny.rb
+++ skinny.rb
@@ -1,12 +1,11 @@
 require "formula"
 
 class Skinny < Formula
-  def skinny_version  
-    "1.3.0"
-  end
+  @skinny_version = "1.3.0"
+
   homepage "http://skinny-framework.org/"
-  url "https://github.com/skinny-framework/skinny-framework/releases/download/#{skinny_version}/skinny-#{skinny_version}.tar.gz"
-  version skinny_version
+  url "https://github.com/skinny-framework/skinny-framework/releases/download/#{@skinny_version}/skinny-#{@skinny_version}.tar.gz"
+  version @skinny_version
   sha1 "d05b6d170e6da0c53fdaac3de10fd643417a9497"
   depends_on "npm"
 
