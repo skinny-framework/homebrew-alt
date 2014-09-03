44fb978fd05972a5e7ebfb8a06d42ee536f9cb3e
Kazuhiro Sera <seratch@gmail.com>
2014-09-03 12:11:33 +0900

    version 1.3.1

--- skinny.rb
+++ skinny.rb
@@ -1,12 +1,12 @@
 require "formula"
 
 class Skinny < Formula
-  @skinny_version = "1.3.0"
+  @skinny_version = "1.3.1"
 
   homepage "http://skinny-framework.org/"
   url "https://github.com/skinny-framework/skinny-framework/releases/download/#{@skinny_version}/skinny-#{@skinny_version}.tar.gz"
   version @skinny_version
-  sha1 "d05b6d170e6da0c53fdaac3de10fd643417a9497"
+  sha1 "336cbfc19497b6eef3b95e1b9c782047ab1ad99b"
   depends_on "npm"
 
   def install
