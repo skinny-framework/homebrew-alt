b6a28e21d2ff0e78041e3cd1c9191f369708abaa
Kazuhiro Sera <seratch@gmail.com>
2014-09-19 00:55:24 +0900

    vesion 1.3.2

--- skinny.rb
+++ skinny.rb
@@ -1,12 +1,12 @@
 require "formula"
 
 class Skinny < Formula
-  @skinny_version = "1.3.1"
+  @skinny_version = "1.3.2"
 
   homepage "http://skinny-framework.org/"
   url "https://github.com/skinny-framework/skinny-framework/releases/download/#{@skinny_version}/skinny-#{@skinny_version}.tar.gz"
   version @skinny_version
-  sha1 "336cbfc19497b6eef3b95e1b9c782047ab1ad99b"
+  sha1 "d265541b96b24a9e1dd440a5702279d6425b8518"
   depends_on "npm"
 
   def install
