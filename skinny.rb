13ec56b90930875f5aca53481a25c60b70380905
Kazuhiro Sera <seratch@gmail.com>
2014-10-20 10:18:44 +0900

    Fix for review commnets

--- skinny.rb
+++ skinny.rb
@@ -1,21 +1,26 @@
 require "formula"
 
 class Skinny < Formula
-  @skinny_version = "1.3.4"
-
   homepage "http://skinny-framework.org/"
-  url "https://github.com/skinny-framework/skinny-framework/releases/download/#{@skinny_version}/skinny-#{@skinny_version}.tar.gz"
+  url "https://github.com/skinny-framework/skinny-framework/releases/download/1.3.4/skinny-1.3.4.tar.gz"
   sha1 "79cb32e4ddc579913445e3514cde62bd6ce66546"
   depends_on "node"
 
+  # TODO Needs to check npm is installed; could be built without-npm
+  option "without-npm", "npm will not be installed"
+
   def install
-    libexec.install Dir['*']
+    libexec.install Dir["*"]
     bin.write_exec_script libexec/"skinny"
   end
 
   def post_install
-    system 'npm install -g yo'
-    system 'npm install -g generator-skinny'
+    # brew install skinny --without-npm
+    return if build.without? "npm"
+
+    system "npm", "install", "-g", "yo"
+    system "npm", "install", "-g", "generator-skinny"
   end
 
 end
+
